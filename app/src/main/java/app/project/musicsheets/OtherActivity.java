bcb7911c31a125c4aafe9f82bd7d5b0f97f73457
engineeringdrew <https://github.com/engineeringdrew/Music-Sheets>
2015-04-11 23:29:12 -0400

    added 2 activities

---
+++ app/src/main/java/app/project/musicsheets/OtherActivity.java
@@ -0,0 +1,18 @@
+package app.project.musicsheets;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Drew on 4/9/2015.
+ */
+public class OtherActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.other_activity);
+
+
+    }
+}

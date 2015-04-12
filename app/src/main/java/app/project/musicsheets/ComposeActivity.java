7574f05528c7f175fb73f945fac843cc58a10242
Jacob Heishman <jrheis01@gmail.com>
2015-04-11 21:01:12 -0400

    Added images and scrollable Linear Layout Added Compose Activity Started Implementing Drag and Drop

--- app/src/main/java/app/project/musicsheets/ComposeActivity.java
+++ app/src/main/java/app/project/musicsheets/ComposeActivity.java
@@ -1,6 +1,5 @@
 package app.project.musicsheets;
 
-import android.content.ClipData;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.DragEvent;
@@ -12,7 +11,7 @@ import android.widget.ImageView;
  */
 public class ComposeActivity extends ActionBarActivity implements View.OnDragListener, View.OnLongClickListener{
 
-    ImageView imageView = new ImageView(this);
+//    ImageView imageView = new ImageView(this);
 /*
     imageView.setOnLongClickListener(new View.OnLongClickListener()
     {
@@ -33,6 +32,52 @@ public class ComposeActivity extends ActionBarActivity implements View.OnDragLis
         setContentView(R.layout.compose_activity);
 
 
+/*
+        // Set touch listeners for each image view
+        // TODO: Fix so that each image view is progammatically assigned Touch Listener
+
+        findViewById(R.id.imageView).setOnTouchListener(new myTouchListener());
+        findViewById(R.id.imageView2).setOnTouchListener(new myTouchListener());
+        findViewById(R.id.imageView3).setOnTouchListener(new myTouchListener());
+        findViewById(R.id.imageView4).setOnTouchListener(new myTouchListener());
+        findViewById(R.id.imageView5).setOnTouchListener(new myTouchListener());
+        findViewById(R.id.imageView6).setOnTouchListener(new myTouchListener());
+        findViewById(R.id.imageView7).setOnTouchListener(new myTouchListener());
+        findViewById(R.id.imageView8).setOnTouchListener(new myTouchListener());
+        findViewById(R.id.imageView9).setOnTouchListener(new myTouchListener());
+        findViewById(R.id.imageView10).setOnTouchListener(new myTouchListener());
+        findViewById(R.id.imageView11).setOnTouchListener(new myTouchListener());
+        findViewById(R.id.imageView12).setOnTouchListener(new myTouchListener());
+        findViewById(R.id.imageView13).setOnTouchListener(new myTouchListener());
+        findViewById(R.id.imageView14).setOnTouchListener(new myTouchListener());
+        findViewById(R.id.imageView15).setOnTouchListener(new myTouchListener());
+        findViewById(R.id.imageView16).setOnTouchListener(new myTouchListener());
+        findViewById(R.id.imageView17).setOnTouchListener(new myTouchListener());
+        findViewById(R.id.imageView18).setOnTouchListener(new myTouchListener());
+*/
+        // Set drag listeners for each image view
+        // TODO: Fix so that each image view is progammatically assigned Drag Listener
+/*
+        findViewById(R.id.imageView).setOnDragListener(new MyDragListener());
+        findViewById(R.id.imageView2).setOnDragListener(new MyDragListener(findViewById()));
+        findViewById(R.id.imageView3).setOnDragListener(new MyDragListener(findViewById(R.drawable.imageView)));
+        findViewById(R.id.imageView4).setOnDragListener(new MyDragListener(findViewById(R.drawable.imageView)));
+        findViewById(R.id.imageView5).setOnDragListener(new MyDragListener(findViewById(R.drawable.imageView)));
+        findViewById(R.id.imageView6).setOnDragListener(new MyDragListener(findViewById(R.drawable.imageView)));
+        findViewById(R.id.imageView7).setOnDragListener(new MyDragListener(findViewById(R.drawable.imageView)));
+        findViewById(R.id.imageView8).setOnDragListener(new MyDragListener(findViewById(R.drawable.imageView)));
+        findViewById(R.id.imageView9).setOnDragListener(new MyDragListener(findViewById(R.drawable.imageView)));
+        findViewById(R.id.imageView10).setOnDragListener(new MyDragListener(findViewById(R.drawable.imageView)));
+        findViewById(R.id.imageView11).setOnDragListener(new MyDragListener(findViewById(R.drawable.imageView)));
+        findViewById(R.id.imageView12).setOnDragListener(new MyDragListener(findViewById(R.drawable.imageView)));
+        findViewById(R.id.imageView13).setOnDragListener(new MyDragListener(findViewById(R.drawable.imageView)));
+        findViewById(R.id.imageView14).setOnDragListener(new MyDragListener(findViewById(R.drawable.imageView)));
+        findViewById(R.id.imageView15).setOnDragListener(new MyDragListener(findViewById(R.drawable.imageView)));
+        findViewById(R.id.imageView16).setOnDragListener(new MyDragListener(findViewById(R.drawable.imageView)));
+        findViewById(R.id.imageView17).setOnDragListener(new MyDragListener(findViewById(R.drawable.imageView)));
+        findViewById(R.id.imageView18).setOnDragListener(new MyDragListener(findViewById(R.drawable.imageView)));
+
+*/
     }
 
 
